942a3592f1622920c32b5c92b68f51c0ab8e6e6a
Lee Bousfield <ljbousfield@gmail.com>
2018-10-21 10:26:22 -0500

    Print block publishing progress

--- publish-blocks.sh
+++ publish-blocks.sh
@@ -7,6 +7,7 @@ if [ $# -lt 4 ]; then
     exit 1
 fi
 
+i=0
 while IFS='' read -r blockInner <&11 && IFS='' read -r work <&12 && IFS='' read -r signature <&13; do
     rpcCall="$(jq -nc --argjson blockInner "$blockInner" --arg work "$work" --arg signature "$signature" \
         '$blockInner * { "work": $work } * { "signature": $signature }
@@ -15,10 +16,14 @@ while IFS='' read -r blockInner <&11 && IFS='' read -r work <&12 && IFS='' read 
     rpcResult="$(curl -s --show-error "$4" -d "$rpcCall")"
     error="$(jq -er .error <(echo "$rpcResult"))"
     if [ "$error" = "Fork" ]; then
+        echo
         echo "Encountered a fork for account $(echo "$blockInner" | jq -r .account)" >&2
     elif [ "$error" != "null" ] && [ "$error" != "Old block" ]; then
+        echo
         echo "Encountered unexpected error '$error' from RPC call $rpcCall" >&2
         exit 2
     fi
+    i=$(($i+1))
+    printf "\r$i"
     sleep 0.1
 done 11<"$1" 12<"$2" 13<"$3"
